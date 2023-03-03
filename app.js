4d78507735d6d52e7a82f2a89bae92155937a007
ng6567 <113035573+ng6567@users.noreply.github.com>
2023-03-03 01:51:45 +0100

    model_things

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-
+const Thing = require('./models/Thing');
 
 const app = express();
-const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://Nag:xvizD4muGdDbw4RD@cluster0.qcjcriv.mongodb.net/?retryWrites=true&w=majority', 
 { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -20,10 +22,13 @@ app.use((req, res, next) => {
   });
 
 app.post('/api/stuff', (req, res, next)=>{
-    console.log(req.body);
-    res.status(201).json({
-        message : 'objet créé!'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 app.get('/api/stuff', (req, res, next) => {
