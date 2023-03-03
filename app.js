36a3fc8fd0fe55cf575deaa0f62040785c71f9bd
ng6567 <113035573+ng6567@users.noreply.github.com>
2023-03-03 11:09:34 +0100

    Enregistrement - modification - suppression

--- app.js
+++ app.js
@@ -31,26 +31,22 @@ app.post('/api/stuff', (req, res, next)=>{
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('api/stuff/:id', (req, res, next) =>{
+  Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+  .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
