8d71a5f8783782baf8df2cb494dd25115dc3408a
ng6567 <113035573+ng6567@users.noreply.github.com>
2023-03-04 11:42:55 +0100

    authorization

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,13 +1,14 @@
-const { Router } = require('express');
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/',stuffCtrl.createThing);
-router.put('/:id',stuffCtrl.modifyThing);
-router.delete('/:id',stuffCtrl.deleteThing);
-router.get('/:id',stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+const Router = require('express');
 const express = require ('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
