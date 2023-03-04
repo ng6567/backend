b741da3c95d6beac95b2f7ffc526424d3908b9c6
ng6567 <113035573+ng6567@users.noreply.github.com>
2023-03-04 01:32:15 +0100

    installation authentification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
 
   const bodyParser = require('body-parser');
 
-  app.use('api/stuff', stuffRoutes);
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes)
 
 module.exports = app;
